deef7031348eb61b3342d7b66c19691be74294b0
Kingcedloga <121218458+Kingcedloga@users.noreply.github.com>
2023-11-23 13:56:52 +0000

    top tweets component

---
+++ src/Composants/TopTweets.jsx
@@ -0,0 +1,11 @@
+import {BsStars} from "react-icons/bs";
+
+const TopTweets = () => {
+  return (
+    <div className="top-tweets">
+       <BsStars />
+      </div>
+      );
+      };
+
+    export default TopTweets;
