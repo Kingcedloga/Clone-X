80328e27ea8dbcc82848876a9ab05487e3569372
Kingcedloga <121218458+Kingcedloga@users.noreply.github.com>
2023-11-23 15:12:07 +0000

    tweet action component

---
+++ src/Composants/TweetAction.jsx
@@ -0,0 +1,31 @@
+import favoriteIcon from "./../images/React.svg";
+import retweetIcon from "./../images/Retweet.svg";
+import replyIcon from "./../images/Reply.svg";
+import repliesIcon from "./../images/Replies.svg";
+
+const TweetAction = ({ valueFavoriteIcon, valueRetweetIcon, valueReplyIcon, valueRepliesIcon }) => {
+
+  return (
+    <div className="tweet-actions">
+      <div className="tweet-action">
+        <img src={replyIcon} alt="" />
+        {valueReplyIcon}
+      </div>
+      <div className="tweet-action">
+        <img src={retweetIcon} alt="" />
+        {valueRetweetIcon}
+      </div>
+      <div className="tweet-action">
+        <img src={favoriteIcon} alt="" />
+        {valueFavoriteIcon}
+      </div>
+      <div className="tweet-action">
+        <img src={repliesIcon} alt="" />
+        {valueRepliesIcon}
+      </div>
+    </div>
+  )
+  
+}
+
+export default TweetAction;
